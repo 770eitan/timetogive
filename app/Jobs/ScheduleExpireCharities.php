7434c684543a4d28d077f79e2a22bae6bd33f02e
Yisroel Goldstein <coding4success@gmail.com>
2022-01-30 01:20:13 -0500

    only expire charities every min if any exist

--- app/Jobs/ScheduleExpireCharities.php
+++ app/Jobs/ScheduleExpireCharities.php
@@ -31,6 +31,23 @@ class ScheduleExpireCharities implements ShouldQueue
     public function handle()
     {
         //
+
+        $any = \App\Models\CharityTicker::where(function($query){
+                $query->whereNull('timer_completed_at')->orWhereRaw('(not ("timer_completed_at" > to_timestamp(0)))');
+            })
+            ->where(function($query) {
+                $query->whereRaw('now() >= "timer_expiry_timestamp"')->orWhereRaw('now() >= ("timer_start" + interval \'6 days\')');
+            })
+            ->where('timer_start', '>', new \Carbon\Carbon(0))
+            ->where('total_donation_amount','>',0)
+            ->where('donation_amount','>',0)
+            ->where('tick_frequency','>',0)
+            ->whereIn('tick_frequency_unit', ['sec','mins','hours','days'])
+            ->doesntExist();
+
+        if(!$any){
+            return;
+        }
         
         // \Log::notice("ScheduleExpireCharities::handle - dispatching ExpireCharities delayed...");
         \App\Jobs\ExpireCharities::dispatch()->delay(now()->addMinutes(1));
